6f11fcdc548405c36f06e37e054a0ab2252c718c
naveen93100 <naveen9310097505@gmail.com>
2026-02-12 15:02:13 +0530

    using env

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(),],
+// })
+
+
+
+import { defineConfig,loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+export default defineConfig(({mode})=>{
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+console.log(env)
+  return{
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      port:  Number(env.VITE_PORT), 
+      host:true,
+      strictPort: true,
+      historyApiFallback: true,
+      allowedHosts:[
+        "localhost",
+        "admin.gautamsolar.us"
+      ]
+    }
+  }
 })
